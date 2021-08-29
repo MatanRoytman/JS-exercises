ee17e42f499840b78b4b24404352c0ea054147ec
Matan Roytman <89573242+MatanRoytman@users.noreply.github.com>
2021-08-29 20:08:43 +0300

    Third_exercise
    absolute value

--- First_exercise.js
+++ First_exercise.js
@@ -1,5 +1,4 @@
 const num = 123
 let sum = 0;
- sum +=num%10;
-
-console.log(sum)
+sum +=(Math.floor(num%10))+(Math.floor(num/10)%10)+(Math.floor((num/100)%10));
+console.log (sum);
---
+++ Third_exercise.js
@@ -0,0 +1,12 @@
+const num = -8
+let absolute;
+if (num<0){
+    absolute=num*-1;
+}
+else if(num==0){
+    absolute=0;
+}
+else {
+    absolute= num;
+}
+console.log(absolute);
